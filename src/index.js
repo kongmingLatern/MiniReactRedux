8afc827810c780aa1e492f5f8a810500d0167c71
施颖杰 <1577310596@qq.com>
2022-07-04 20:01:14 +0800

    clean some files

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
 );
-
-
-const reducer = (totalVal, curVal) => totalVal + curVal 
