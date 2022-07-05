5a596ca489dd7fb0db2e49a7ac88d83f155b2a0b
施颖杰 <1577310596@qq.com>
2022-07-05 16:17:38 +0800

    feat: finish redux's logger and thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "../my-redux-nut";
 // import { createStore } from "redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
+// import thunk from "redux-thunk";
+// import logger from "redux-logger";
 // import { applyMiddleware } from "redux";
 
 function countReducer(state = 0, action) {
@@ -16,5 +16,34 @@ function countReducer(state = 0, action) {
 }
 
 const store = createStore(countReducer, applyMiddleware(thunk, logger))
+
+
+function logger({ getState, dispatch }) {
+  return next => action => {
+    const prevState = getState()
+    console.log('--------------');
+    console.log(`${action.type}被执行了`);
+    console.log('prevState:', prevState)
+
+    const returnVal = next(action)
+
+    console.log('--------------')
+    const nextState = getState()
+    console.log('nextState:', nextState)
+
+    return returnVal
+  }
+}
+
+
+function thunk({ getState, dispatch }) {
+  return next => action => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState)
+    }
+    return next(action)
+  }
+}
+
 
 export default store
