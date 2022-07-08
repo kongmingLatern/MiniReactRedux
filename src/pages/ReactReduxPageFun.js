fc29a51e607ceae1292b1a0a127eb6aeab65c122
施颖杰 <1577310596@qq.com>
2022-07-08 21:22:31 +0800

    feat: finish ReactReduxForFun

--- src/pages/ReactReduxPageFun.js
+++ src/pages/ReactReduxPageFun.js
@@ -1,5 +1,19 @@
+import { useCallback } from "react"
+import { useDispatch, useSelector } from "../react-redux"
+
 export default function ReactReduxPageFun() {
+  const count = useSelector(({ count }) => count)
+
+  const dispatch = useDispatch()
+
+  const add = useCallback(() => {
+    dispatch({ type: "ADD" })
+  }, [])
+
   return (
-    <div>ReactReduxPageFun</div>
+    <>
+      <div>ReactReduxPageFun</div>
+      <button onClick={add}>{count}</button>
+    </>
   )
 }
