013b7b5420f60a728dbbb3fff17395dfce9ea527
施颖杰 <1577310596@qq.com>
2022-07-08 21:22:00 +0800

    feat: add useDispatch

--- src/react-redux/useDispatch.js
+++ src/react-redux/useDispatch.js
@@ -1,2 +1,10 @@
+import { useContext } from "react";
+import { Context } from "./Provider";
+
 export default function useDispatch() {
+  const store = useContext(Context)
+
+  const { dispatch } = store
+
+  return dispatch
 };
