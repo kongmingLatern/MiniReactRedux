44d354d9267156650e57dcc1398da77a474acb4d
施颖杰 <1577310596@qq.com>
2022-07-09 20:19:16 +0800

    feat: add a page for testRedux

---
+++ src/pages/ReduxPageTest.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ReduxPageTest extends Component {
+  render() {
+    return (
+      <>
+        <div>ReduxPageTest</div>
+      </>
+    )
+  }
+}
